b48991d81aa1764bd7f663d34cbecdafba3a6f86
wangcong <775804659@qq.com>
2018-05-14 10:21:56 +0800

    sex commit

--- src/main/java/cn/com/bean/User.java
+++ src/main/java/cn/com/bean/User.java
@@ -18,97 +18,128 @@ public class User {
 	private String children;
 	private String idNumber;
 	private String address;
+	private String caseHistory;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public Integer getAge() {
 		return age;
 	}
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+
 	public Date getBirthday() {
 		return birthday;
 	}
+
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
+
 	public String getNation() {
 		return nation;
 	}
+
 	public void setNation(String nation) {
 		this.nation = nation;
 	}
+
 	public String getEducation() {
 		return education;
 	}
+
 	public void setEducation(String education) {
 		this.education = education;
 	}
+
 	public String getMaritalStatus() {
 		return maritalStatus;
 	}
+
 	public void setMaritalStatus(String maritalStatus) {
 		this.maritalStatus = maritalStatus;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getIdNumber() {
 		return idNumber;
 	}
+
 	public void setIdNumber(String idNumber) {
 		this.idNumber = idNumber;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
+
 	public String getChildren() {
 		return children;
 	}
+
 	public void setChildren(String children) {
 		this.children = children;
 	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", password=" + password + ", name=" + name + ", gender="
@@ -116,6 +147,5 @@ public class User {
 				+ ", maritalStatus=" + maritalStatus + ", phone=" + phone + ", email=" + email + ", children="
 				+ children + ", idNumber=" + idNumber + ", address=" + address + "]";
 	}
-
 
 }
